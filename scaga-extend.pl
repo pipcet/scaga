a8fcec89d6130c2af831d47f85bc82a1694140b0
Philip <pipcet@gmail.com>
2015-09-02 00:03:24 +0000

    scaga-extend.pl: .scaga -> .scaga
    accept call paths as input; attempt to extend (concatenate)
           A > B
    and
           B > C
    into
           A > B > C

---
+++ scaga-extend.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use Scaga;
+use Getopt::Long;
+
+my $rules = 'rules.scaga';
+
+GetOptions("rules=s" => \$rules);
+
+my @expansions;
+my @paths;
+
+while (<>) {
+    chomp;
+
+    my $path = Scaga::Path->new($_);
+
+    if ($path->n == 1) {
+        my @ppaths = @{$path->{ppaths}};
+        my $rule = Scaga::Rule->new($ppaths[0]->{patterns}->[0]->repr . " => " . $path->repr);
+        push @expansions, $rule;
+    }
+
+    push @paths, $path;
+}
+
+for my $path (@paths) {
+    for my $expansion (@expansions) {
+        my $m = $path->endmatch($expansion->{in});
+
+        if ($m) {
+            my $newpath = $path->slice(0, $m->[0])->concat($expansion->{out});
+
+            print $newpath->repr . "\n";
+        }
+    }
+}
