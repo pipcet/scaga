7005dbee107ac8ac89444bf630cadd91145c9a19
Philip <pipcet@gmail.com>
2015-09-02 23:07:03 +0000

    --max argument to limit recursion depth

--- scaga-extend-recursively.pl
+++ scaga-extend-recursively.pl
@@ -1,23 +1,29 @@
 #!/usr/bin/perl
 use IPC::Run qw(run new_chunker);
-use File::Slurp qw(read_file);
+use File::Slurp qw(read_file write_file);
 use Getopt::Long;
 use strict;
 
 my $rules_file = 'rules.scaga';
 my $exps_file = 'expansions.scaga';
+my $maxcycles;
 
 GetOptions("rules=s" => \$rules_file,
-           "expansions=s" => \$exps_file);
+           "expansions=s" => \$exps_file,
+           "max=i" => \$maxcycles);
 
 my $in = read_file(\*STDIN);
 my $out;
 
-while (1) {
+my $cycles = 0;
+
+while (!(defined $maxcycles and $cycles >= $maxcycles)) {
+    write_file("ser.iteration.$cycles", $in);
+    $cycles++;
     my %seen;
-    run(["perl", "./scaga-extend.pl", "--rules=$rules_file", "--expansions=$exps_file"], \$in, '>', new_chunker, sub { $seen{$_[0]} = 1; });
+    run(["perl", "./scaga-extend.pl", "--rules=$rules_file", "--expansions=$exps_file"], \$in, '>', new_chunker, sub { chomp $_[0]; $seen{$_[0]} = 1; });
     $out = join("\n", sort keys %seen);
-    warn "in $in out $out";
+    warn "iteration $cycles: " . scalar(keys %seen) . " paths";
     last if $in eq $out;
     $in = $out;
 }
